d69e3e47d74c6af7eeda528a8e351019827e8975
siddhant1 <sidhant.manchanda@gmail.com>
2019-05-22 01:11:34 +0530

    Refactor App

--- movies/models.py
+++ movies/models.py
@@ -11,4 +11,4 @@ class Movie(models.Model):
     release_year = models.IntegerField()
     number_in_stock=models.IntegerField()
     daily_rate=models.FloatField()
-    genre:models.ForeignKey(Genre,on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre,on_delete=models.CASCADE)
